fa60c3f431b13e3ae9754629a912f25f7cb83fb7
황창순 <javasoon2@hwang-ui-macbook-pro.local>
2016-02-22 19:56:38 +0900

    modify

--- UBISEcallFR01/ViewController.m
+++ UBISEcallFR01/ViewController.m
@@ -35,7 +35,7 @@ NSMutableArray *beaconList;
 NSMutableArray *beaconBatteryLevelList;
 int seqBeacon = 0;
 int beaconSkeepCount = 0;
-int beaconSkeepMaxCount = 3;
+int beaconSkeepMaxCount = 1;
 
 CLBeaconRegion *beaconRegion;
 
